8dd9c868b08575cfad6d0445115b5d53067331e3
Zdenek Rovnez <80111494+ZdenekRovnez@users.noreply.github.com>
2024-02-29 19:03:57 +0100

    added extra git function powershell

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,5 @@
+### pip wrapper
+
 # Get the directory of the current PowerShell profile
 $profileDir = Split-Path $PROFILE
 
@@ -11,12 +13,43 @@ function dgit {
     & git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME @args
 }
 
+### git info
 
+function git-info {
+    param (
+        [string]$useDgit
+    )
 
+    $gitCommand = "git"
 
+    # use optional argument 'dgit' to use alias 'dgit' 
+    if ($useDgit -eq "dgit") {
+        $gitCommand = "dgit"
+    }
+
+    Write-Output "Repository Summary:"
+    Write-Output "--------------------------------------"
+    Write-Output "Branches:"
+    & $gitCommand branch -a
+    Write-Output "--------------------------------------"
+    Write-Output "Last 5 Commits:"
+    & $gitCommand log --oneline -n 5
+    Write-Output "--------------------------------------"
+    Write-Output "Repository Status:"
+    & $gitCommand status -s
+    Write-Output "--------------------------------------"
+    Write-Output "Remote Repositories:"
+    & $gitCommand remote -v
+}
+
+### Miscellaneous
+
+# cheat function (cht)
 function cht {
     param(
         [string]$argument
     )
     curl "https://cheat.sh/$argument"
 }
+
+
